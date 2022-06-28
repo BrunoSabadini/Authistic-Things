5af3cf6e7ed88cd5135af4c98c0961f5099f8904
BrunoSabadini <bruno_sabadini@hotmail.com>
2022-06-28 14:27:05 -0300

    Initial Commit

---
+++ autistic things/autistic_thingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  autistic_thingsApp.swift
+//  autistic things
+//
+//  Created by Bruno Sabadini on 28/06/22.
+//
+
+import SwiftUI
+
+@main
+struct autistic_thingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
