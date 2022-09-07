f9a5db5600dc605d0bd746580fe406b4df9ebe2f
BrunoSabadini <bruno_sabadini@hotmail.com>
2022-09-07 09:49:05 -0300

    Lot of bug fix

--- autistic things/ContentView.swift
+++ autistic things/ContentView.swift
@@ -6,19 +6,6 @@
 //
 import SwiftUI
 
-struct QuestionsModelView:View{
-  
-  let questionModel: QuestionsModel
-  @State var checkQuestion: Bool = false
-  
-  var body: some View{
-    Toggle(isOn: $checkQuestion) {
-      Text(questionModel.question)
-    }.toggleStyle(CheckboxStyle())
-      .onAppear(perform: {checkQuestion = questionModel.check
-      } )
-  }
-}
 
 class QuestionsModel: Identifiable {
   
@@ -37,56 +24,64 @@ class QuestionsData: ObservableObject{
   
   func addQuestion(question: String){
     boolArr.append(QuestionsModel(question: question, check: false))
+    boolArr.append(QuestionsModel(question: "aaaaaaafff", check: false))
+
+    print(boolArr.count)
   }
+  func delete(at offsets: IndexSet) {
+          boolArr.remove(atOffsets: offsets)
+      }
 }
 
 struct MyView: View {
   
   @StateObject var questionData: QuestionsData = QuestionsData()
-  
+ 
      var body: some View {
-       NavigationView{
-       VStack{
-             VStack {
-               List{
+       NavigationView {
+         VStack{
+           VStack {
+             List{
                ForEach($questionData.boolArr) { $firstObject in
                  Toggle(isOn: $firstObject.check) {
                    Text(firstObject.question)
-                                 }.toggleStyle(CheckboxStyle())}
-               }}.navigationTitle("Questions")
-         Spacer()
-         HStack{
-             NavigationLink(destination:  AddNewQuestion()) {
+                 }.toggleStyle(CheckboxStyle())}.onDelete(perform: questionData.delete)
+             }
+           }
+           .navigationTitle("Questions")
+           Spacer()
+           HStack{
+             NavigationLink(destination:  {
+               AddNewQuestion()
+                 .environmentObject(questionData)
+             }) {
                Text("Add")
              }
-         Spacer()
-         Button("Remove") {
+           }
          }
-         }}.environmentObject(questionData)
+         .environmentObject(questionData)
        }
      }
 }
 
 struct AddNewQuestion: View {
   
-  @State var name: String = ""
+  @State var question: String = ""
   @EnvironmentObject var questionData: QuestionsData
-  
+  @Environment(\.dismiss) var dismiss
   var body: some View {
     VStack{
       Spacer()
       List{
-        TextField("Type the question here", text: $name)
-        NavigationLink(destination:  MyView()) {
+        TextField("Type the question here", text: $question)
           Button("Save") {
-            questionData.addQuestion(question: name)
-            print(name)
-          }
+            if question.count > 0{
+                questionData.boolArr.append(QuestionsModel(question: question, check: false))}
+            dismiss()
         }}
       Spacer()
     }
-  }
-}
+  }}
   
   struct CheckBoxView_Previews: PreviewProvider {
     struct CheckBoxViewHolder: View {
@@ -99,5 +94,6 @@ struct AddNewQuestion: View {
     
     static var previews: some View {
       MyView()
+      AddNewQuestion()
     }
   }
